4c983c469d8e9e7205009df5c14f2bf09b069986
ejaakml <eximindotong@yahoo.com>
2020-10-20 17:55:59 +0700

    x

---
+++ src/pboif2/pkg10119061/latihan13/relational/PBOIF210119061Latihan13Relational.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119061.latihan13.relational;
+
+/**
+ *
+ * @author Rheiza
+ */
+public class PBOIF210119061Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int bill = 10;
+       int bill2 = 20;
+       System.out.println("a == b = " + (bill == bill2));
+       System.out.println("a != b = " + (bill != bill2));
+       System.out.println("a > b = " + (bill > bill2));
+       System.out.println("a < b = " + (bill < bill2));
+       System.out.println("b >= a = " + (bill2 >= bill));
+       System.out.println("b <= a = " + (bill2 <= bill));
+    }
+    
+}
